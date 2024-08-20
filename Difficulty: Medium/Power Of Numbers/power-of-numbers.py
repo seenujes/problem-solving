e2441a4e554713c8018997e782f06d980ac22aea
seenu_prince <119055907+seenujes@users.noreply.github.com>
2024-08-20 06:19:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Power Of Numbers/power-of-numbers.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+
+class Solution:
+    #Complete this function
+    def power(self,N,R):
+        #Your code here
+        return int(pow(N,R,1000000007))
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        N=input()
+        R=N[::-1]
+        
+        ob=Solution();
+        ans=ob.power(int(N),int(R))
+        print(ans)
+        
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
