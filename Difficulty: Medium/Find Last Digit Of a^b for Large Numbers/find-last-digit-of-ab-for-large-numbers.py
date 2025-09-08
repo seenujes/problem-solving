c3da49e71979f99c3d6d35b73fdf4292324dc272
seenu_prince <119055907+seenujes@users.noreply.github.com>
2025-09-08 11:48:18 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find Last Digit Of a^b for Large Numbers/find-last-digit-of-ab-for-large-numbers.py
@@ -0,0 +1,8 @@
+#User function Template for python3
+
+class Solution:
+    def getLastDigit(self, a, b):
+        # code here 
+        # value = pow(int(a),int(b))
+        # return value%10
+        return pow(int(a),int(b),10)
